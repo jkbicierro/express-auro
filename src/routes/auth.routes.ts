60ee5bc6254b8cfc736f927029e4cfcf0b99e447
John Bicierro <61955888+jkbicierro@users.noreply.github.com>
2025-03-25 00:46:17 +0000

    added simulated post request for authentication

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,36 @@
+/*
+    Feature: Authentication
+    Purpose: HTTP Request
+
+    Issues (3/25/25):
+        [POST] /auth/signup 
+            - Change the simulated data to database integration
+            - Hash the password before storing
+        [POST] /auth/login
+            - Compare req.body (email, password) in stored data (database) including hashing algorithm
+            - Give session & token for security purposes
+        
+*/
 
 import { Router } from "express";
 const router = Router();
+
+router.post("/auth/login", (req: Request, res: Response) => {
+    try {
+         const { email, password } = req.body;
+
+         if(!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+         }
+
+
+    }
+    catch(err)
+    {
+        console.error("[POST] router /auth/login:", err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
 
 router.post("/auth/signup", (req: Request, res: Response) => {
     try {
