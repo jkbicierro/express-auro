5df44c3cb539f8166525fddf3168f472649158a8
Jeck <zrkeimperial@gmail.com>
2025-05-06 10:56:45 +0800

    remove build cache

--- dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_middleware_1 = require("../middleware/auth.middleware");
-const user_controller_1 = require("../controllers/user.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get("/getalluser", auth_middleware_1.AuthenticateToken, user_controller_1.GetAllUser);
-router.get("/getuser", auth_middleware_1.AuthenticateToken, user_controller_1.GetUser);
-exports.default = router;
